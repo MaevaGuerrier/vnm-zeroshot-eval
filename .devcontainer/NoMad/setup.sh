e130a00fef58b97d49ac2e7190f8241b22c9e370
Bunker2 <vivekshankarv@gmail.com>
2025-08-24 17:43:35 -0400

    some changes for crossformer deploy

--- .devcontainer/NoMad/setup.sh
+++ .devcontainer/NoMad/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /opt/ros/noetic/setup.bash
-source /workspace/ros1_ws/devel/setup.bash
-export PYTHONPATH=/workspace/.packages_nomad:$PYTHONPATH
-alias bunker_server='roslaunch bunker_robot_server bunker_server.launch'
+# source /opt/ros/noetic/setup.bash
+# source /workspace/ros1_ws/devel/setup.bash
+# export PYTHONPATH=/workspace/.packages_nomad:$PYTHONPATH
+# alias bunker_server='roslaunch bunker_robot_server bunker_server.launch'
 exec "$@"
