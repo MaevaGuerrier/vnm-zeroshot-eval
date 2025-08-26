82bda8b1e477e444bec533f7ef46ae386587f2eb
MaevaGuerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-08-26 09:54:48 -0400

    udpate remotevis

--- .devcontainer/remotevisualizer/setup.sh
+++ .devcontainer/remotevisualizer/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-source /opt/ros/noetic/setup.bash
-
-export ROS_MASTER_URI=http://192.168.1.23:11311
 
 
+# source /opt/ros/noetic/setup.bash
+source /opt/ros/humble/setup.bash
+# export ROS_MASTER_URI=http://192.168.1.178:11311 # noetic
 
-exec rviz -d /workspace/.devcontainer/remotevisualizer/config.rviz
+# export ROS_DOMAIN_ID=10 # TODO DO IT ON THE ROBOT AS WELL
+
+
+# exec rviz -d /workspace/.devcontainer/remotevisualizer/config.rviz # noetic
+exec /bin/bash
