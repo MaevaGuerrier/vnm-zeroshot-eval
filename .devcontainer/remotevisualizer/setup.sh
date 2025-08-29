17ffc3965497a7fe17e268444d4c06eea3689141
MaevaGuerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-08-29 16:46:24 -0400

    update remote viz test_cross

--- .devcontainer/remotevisualizer/setup.sh
+++ .devcontainer/remotevisualizer/setup.sh
@@ -5,12 +5,12 @@ source /opt/ros/noetic/setup.bash
 export ROS_MASTER_URI=http://192.168.1.178:11311
 
 
-# source /opt/ros/noetic/setup.bash
-source /opt/ros/humble/setup.bash
-# export ROS_MASTER_URI=http://192.168.1.178:11311 # noetic
+source /opt/ros/noetic/setup.bash
+# source /opt/ros/humble/setup.bash
+export ROS_MASTER_URI=http://192.168.1.178:11311 # noetic
 
 # export ROS_DOMAIN_ID=10 # TODO DO IT ON THE ROBOT AS WELL
 
 
-# exec rviz -d /workspace/.devcontainer/remotevisualizer/config.rviz # noetic
+exec rviz -d /workspace/.devcontainer/remotevisualizer/config.rviz # noetic
 exec /bin/bash
