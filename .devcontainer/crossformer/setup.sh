76b963a37f4b9efa0dccaee4b220c31c9689ecc4
Bunker1 <vivekshanakrv@gmail.com>
2025-09-04 15:01:45 -0400

    some minor modifications in SafeGNM

--- .devcontainer/crossformer/setup.sh
+++ .devcontainer/crossformer/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # source ~/.bashrc
-
+source /opt/ros/humble/setup.bash
+source /workspace/ros2_ws/install/setup.bash
+export PYTHONPATH=$CONDA_PREFIX/lib/python3.10/site-packages:$PYTHONPATH
+export ROS_DOMAIN_ID=20
 
 exec "$@"
