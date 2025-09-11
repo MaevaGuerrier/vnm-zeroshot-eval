829c874c2ff5d5bf45378db6338c57c37708cb1c
MaevaGuerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-09-11 18:17:12 -0400

    crossformer only submodules

--- .devcontainer/CARE/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /opt/ros/humble/setup.bash
-source /workspace/ros2_ws/install/setup.bash
-export PYTHONPATH=/workspace/.packages_care:$PYTHONPATH
-exec "$@"
--- .devcontainer/NoMad/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/humble/setup.bash
-source /workspace/ros2_ws/install/setup.bash
-export PYTHONPATH=/workspace/.packages_crossformer:$PYTHONPATH
-alias bunker_server='ros2 launch bunker_robot_server bunker_robot_server.launch'
-exec "$@"
