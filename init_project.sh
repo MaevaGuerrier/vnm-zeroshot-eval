7c60bca9537aea0aa0ae601d5c42e87489c6744e
MaevaGuerrier <simonroy99@hotmail.ca>
2025-11-24 18:39:21 -0500

    init bunker naivibridger

--- init_project.sh
+++ init_project.sh
@@ -7,16 +7,3 @@ pip3 install /workspace/third_party/diffusion_policy/ --target $target
 
 pip3 install /workspace/src/NaiviBridger/train/ --target $target
 
-# pip3 install rosnumpy --target $target
-
-# pip3 install --upgrade attrs --target $target
-
-# pip3 install --upgrade --force-reinstall numpy --target $target
-
-# pip3 install efficientnet-pytorch --target $target
-
-# pip3 install einops --target $target
-
-# pip3 install vit-pytorch --target $target
-
-# pip3 install diffusers==0.11.1 --target $target
