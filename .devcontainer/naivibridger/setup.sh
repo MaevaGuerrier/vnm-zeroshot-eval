4eb390c1d8a167b0d7056d804c444fce464fef41
Maeva Guerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-11-26 11:15:06 -0500

    Update ROS_MASTER_URI and ROS_IP in setup.sh

--- .devcontainer/naivibridger/setup.sh
+++ .devcontainer/naivibridger/setup.sh
@@ -4,4 +4,9 @@
 target="/workspace/.packages_naivibridger"
 source /opt/ros/noetic/setup.bash
 export PYTHONPATH=${target}:$PYTHONPATH
+
+# export ROS_MASTER_URI=http://192.168.1.178:11311 # BUNKER1
+export ROS_MASTER_URI=http://192.168.1.141:11311 # BUNKER2 ASLAN
+export ROS_IP=192.168.1.141
+
 exec "$@"
