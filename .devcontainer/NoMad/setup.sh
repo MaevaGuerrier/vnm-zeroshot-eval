3fa885f2ea3fa2f467a7b80e714ff0d190455a0c
Maeva Guerrier <87130407+MaevaGuerrier@users.noreply.github.com>
2025-11-26 11:14:07 -0500

    + ros_master aslan

--- .devcontainer/NoMad/setup.sh
+++ .devcontainer/NoMad/setup.sh
@@ -2,6 +2,10 @@
 
 source /opt/ros/noetic/setup.bash
 # source /workspace/ros1_ws/install/setup.bash
-export PYTHONPATH=/workspace/.packages_nomad:$PYTHONPATH
+target="/workspace/.packages_nomad"
+export PYTHONPATH=$target:$PYTHONPATH
+# export ROS_MASTER_URI=http://192.168.1.178:11311 # BUNKER1
+export ROS_MASTER_URI=http://192.168.1.141:11311 # BUNKER2 ASLAN
+export ROS_IP=192.168.1.141
 exec "$@"
     
