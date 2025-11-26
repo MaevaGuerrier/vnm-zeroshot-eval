33a68603d29fcc90618db641924affd6e1b7d22e
MaevaGuerrier <simonroy99@hotmail.ca>
2025-11-26 12:53:00 -0500

    +aslan ip change

--- .devcontainer/NoMad/setup.sh
+++ .devcontainer/NoMad/setup.sh
@@ -5,7 +5,7 @@ source /opt/ros/noetic/setup.bash
 target="/workspace/.packages_nomad"
 export PYTHONPATH=$target:$PYTHONPATH
 # export ROS_MASTER_URI=http://192.168.1.178:11311 # BUNKER1
-export ROS_MASTER_URI=http://192.168.1.141:11311 # BUNKER2 ASLAN
-export ROS_IP=192.168.1.141
+export ROS_MASTER_URI=http://192.168.1.23:11311 # BUNKER2 ASLAN
+export ROS_IP=192.168.1.23
 exec "$@"
     
