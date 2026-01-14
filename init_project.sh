d9fc525908b81b7c3719d6b8d4731406fda040dc
soma <soma.nitt@gmail.com>
2026-01-14 11:08:53 -0500

    defined deploy packages for ros1 & 2

--- init_project.sh
+++ init_project.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-target_dir=/workspace/.packages_nomad
+target_dir=/workspace/.packages_nomad_ros2
 
 # install diffusion_policy
 pip install third_party/diffusion_policy/ --target=$target_dir
 pip install src/visualnav-transformer/train --target=$target_dir
-
-# install visualnav_transformer
-# pip install src/visualnav-transformer/train/ --target=$target_dir
-# pip install crossformer/ --target=$target_dir
-# pip install robo-gym/ --target=$target_dir 
-# pip install "scipy==1.12"
-
-# build sim_ws
-#cd sim_ws/
-#catkin build 
-#source devel/setup.bash
+pip install src/visualnav-transformer/deployment --target=$target_dir
 
 cd /workspace
