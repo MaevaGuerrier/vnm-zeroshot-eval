35311a252e2ad0f02f458a57193db72710c0d315
soma <soma.nitt@gmail.com>
2026-01-23 15:18:09 -0500

    ros2 debug done

--- init_project.sh
+++ init_project.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-target_dir="/workspace/.packages_naivibridger_ros2" # the var is also set in the dockerfile setup.sh we could remove here
-echo "Target dir is set to $target_dir"
-pip install /workspace/third_party/diffusion_policy/ --target=$target_dir
-pip install /workspace/src/NaiviBridger/deployment/ --target=$target_dir
-pip install /workspace/src/NaiviBridger/train/ --target=$target_dir
+
+# CAREFUL MAKE SURE TARGET DIR IS CORRECT
+target_dir="/workspace/.packages_naivibridger_ros2"
+
+# We had issues when changing package contents, remaining build files would interfere when installing again.
+# Clean everything
+echo "Cleaning all build artifacts"
+rm -rf "$target_dir" # CAREFUL MAKE SURE TARGET DIR IS CORRECT
+find /workspace/src/NaiviBridger/ -type d \( -name "build" -o -name "dist" -o -name "*.egg-info" -o -name "__pycache__" \) -exec rm -rf {} + 2>/dev/null
+
+
+echo "Installing packages to $target_dir"
+pip install /workspace/third_party/diffusion_policy/ --target="$target_dir"
+pip install /workspace/src/NaiviBridger/deployment/ --target="$target_dir"
+pip install /workspace/src/NaiviBridger/train/ --target="$target_dir"
