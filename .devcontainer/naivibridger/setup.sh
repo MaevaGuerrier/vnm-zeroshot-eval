1f5a3564f04cc37367373b3f8222fe4a905095ce
soma <soma.nitt@gmail.com>
2026-01-23 13:24:24 -0500

    progress ros2

--- .devcontainer/naivibridger/setup.sh
+++ .devcontainer/naivibridger/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # source ~/.bashrc
 # source 
-target="/workspace/.packages_naivibridger"
-source /opt/ros/noetic/setup.bash
+target_dir="/workspace/.packages_naivibridger_ros2"
+source /opt/ros/humble/setup.bash
 export PYTHONPATH=${target}:$PYTHONPATH
 
 # export ROS_MASTER_URI=http://192.168.1.178:11311 # BUNKER1
-export ROS_MASTER_URI=http://192.168.1.139:11311 # BUNKER2 ASLAN
-export ROS_IP=192.168.1.139
+# export ROS_MASTER_URI=http://192.168.1.139:11311 # BUNKER2 ASLAN
+# export ROS_IP=192.168.1.139
 
 exec "$@"
--- init_project.sh
+++ init_project.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-target=/workspace/.packages_naivibridger
-
-pip3 install /workspace/third_party/diffusion_policy/ --target $target
-
-
-pip3 install /workspace/src/NaiviBridger/train/ --target $target
-
+target_dir="/workspace/.packages_naivibridger_ros2" # the var is also set in the dockerfile setup.sh we could remove here
+echo "Target dir is set to $target_dir"
+pip install third_party/diffusion_policy/ --target=$target_dir
+pip install src/NaiviBridger/deployment --target=$target_dir
+pip install src/NaiviBridger/train/ --target=$target_dir
