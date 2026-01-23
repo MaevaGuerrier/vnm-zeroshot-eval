470b5cf37f1f147dd31ee10cbeccfe4524703a72
soma <soma.nitt@gmail.com>
2026-01-23 14:29:41 -0500

    progress ros2 issue packages

--- .devcontainer/naivibridger/setup.sh
+++ .devcontainer/naivibridger/setup.sh
@@ -3,7 +3,7 @@
 # source 
 target_dir="/workspace/.packages_naivibridger_ros2"
 source /opt/ros/humble/setup.bash
-export PYTHONPATH=${target}:$PYTHONPATH
+export PYTHONPATH=${target_dir}:$PYTHONPATH
 
 # export ROS_MASTER_URI=http://192.168.1.178:11311 # BUNKER1
 # export ROS_MASTER_URI=http://192.168.1.139:11311 # BUNKER2 ASLAN
--- init_project.sh
+++ init_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 target_dir="/workspace/.packages_naivibridger_ros2" # the var is also set in the dockerfile setup.sh we could remove here
 echo "Target dir is set to $target_dir"
-pip install third_party/diffusion_policy/ --target=$target_dir
-pip install src/NaiviBridger/deployment --target=$target_dir
-pip install src/NaiviBridger/train/ --target=$target_dir
+pip install /workspace/third_party/diffusion_policy/ --target=$target_dir
+pip install /workspace/src/NaiviBridger/deployment/ --target=$target_dir
+pip install /workspace/src/NaiviBridger/train/ --target=$target_dir
